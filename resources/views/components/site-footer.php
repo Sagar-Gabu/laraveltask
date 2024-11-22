f15f7251d8f4d755dad75894b03ed45cbc633c65
sagar <mrsagargabu@gmail.com>
2024-11-22 14:13:17 +0530

    add showproduct by category

--- resources/views/components/site-footer.php
+++ resources/views/components/site-footer.php
@@ -1,13 +1,13 @@
 <footer class="bg-dark text-light py-4 mt-5">
     <div class="container">
         <div class="row">
-            <!-- About Us Section -->
+    
             <div class="col-md-4 mb-3">
                 <h5>About Us</h5>
                 <p>my shop is an e-commerce platform offering a wide variety of products across different categories. We aim to provide the best shopping experience for our customers.</p>
             </div>
 
-            <!-- Quick Links Section -->
+
             <div class="col-md-4 mb-3">
                 <h5>Quick Links</h5>
                 <ul class="list-unstyled">
@@ -17,7 +17,7 @@
                 </ul>
             </div>
 
-            <!-- Contact Information Section -->
+        
             <div class="col-md-4 mb-3">
                 <h5>Contact Us</h5>
                 <p>Email: support@yourstore.com</p>
@@ -25,7 +25,7 @@
             </div>
         </div>
 
-        <!-- Social Media Section -->
+        
         <div class="text-center mt-4">
             <a href="#" class="text-light mx-2"><i class="fab fa-facebook-f"></i></a>
             <a href="#" class="text-light mx-2"><i class="fab fa-twitter"></i></a>
@@ -33,7 +33,7 @@
             <a href="#" class="text-light mx-2"><i class="fab fa-linkedin-in"></i></a>
         </div>
 
-        <!-- Copyright -->
+        
         <div class="text-center mt-4">
             <p>&copy; 2024 myshop. All Rights Reserved.</p>
         </div>
